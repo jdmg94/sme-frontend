3244385626e16ce8d497dacd7faf564b74b7c83d
Jose Munoz <jdmg93@gmail.com>
2021-02-17 21:23:20 -0800

    minor fix

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@
  */
 import React from 'react';
 import store from './store';
-import App from './containers/App';
+import App from './components/App';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import registerServiceWorker from './registerServiceWorker';
